1f898e5e26c56d2b6de0d6bff9a9f2ed9bc66051
Mafra <matheuslpm@gmail.com>
2016-10-11 14:35:25 -0300

    Perfil com a foto circular

--- app/src/main/java/br/unifor/retail/Perfil.java
+++ app/src/main/java/br/unifor/retail/Perfil.java
@@ -1,6 +1,9 @@
 package br.unifor.retail;
 
 import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Path;
+import android.graphics.Rect;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
@@ -17,17 +20,44 @@ public class Perfil extends AppCompatActivity {
         setContentView(R.layout.activity_perfil);
 
         //extraemos el drawable en un bitmap
-        Drawable originalDrawable = getResources().getDrawable(R.drawable.felipemassapilotof1);
+        Drawable originalDrawable = getResources().getDrawable(R.drawable.pandalambendo);
         Bitmap originalBitmap = ((BitmapDrawable) originalDrawable).getBitmap();
         //creamos el drawable redondeado
-        RoundedBitmapDrawable roundedDrawable = RoundedBitmapDrawableFactory.create(getResources(), originalBitmap);
+
+        getRoundedShape(originalBitmap);
+        RoundedBitmapDrawable roundedDrawable = RoundedBitmapDrawableFactory.create(getResources(), getRoundedShape(originalBitmap));
 
         //asignamos el CornerRadius
-        roundedDrawable.setCornerRadius(originalBitmap.getWidth());
-        roundedDrawable.setCircular(true);
+        //roundedDrawable.setCornerRadius(originalBitmap.getWidth());
+        //roundedDrawable.setCircular(true);
+
+
 
         ImageView imageView = (ImageView) findViewById(R.id.imagem_perfil);
 
         imageView.setImageDrawable(roundedDrawable);
+    }
+
+    public Bitmap getRoundedShape(Bitmap scaleBitmapImage) {
+        int targetWidth = 200;
+        int targetHeight = 200;
+        Bitmap targetBitmap = Bitmap.createBitmap(targetWidth,
+                targetHeight,Bitmap.Config.ARGB_8888);
+
+        Canvas canvas = new Canvas(targetBitmap);
+        Path path = new Path();
+        path.addCircle(((float) targetWidth - 1) / 2,
+                ((float) targetHeight - 1) / 2,
+                (Math.min(((float) targetWidth),
+                        ((float) targetHeight)) / 2),
+                Path.Direction.CCW);
+
+        canvas.clipPath(path);
+        Bitmap sourceBitmap = scaleBitmapImage;
+        canvas.drawBitmap(sourceBitmap,
+                new Rect(0, 0, sourceBitmap.getWidth(),
+                        sourceBitmap.getHeight()),
+                new Rect(0, 0, targetWidth, targetHeight), null);
+        return targetBitmap;
     }
 }
